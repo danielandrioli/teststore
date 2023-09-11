7d21dac42869ff729c392306695d7a14dcc7e929
Daniel Andrioli <andrioli.daniel@yahoo.com.br>
2023-09-11 11:59:45 -0300

    Initial dependencies

---
+++ src/main/java/com/daniboy/util/Constants.java
@@ -0,0 +1,8 @@
+package com.daniboy.util;
+
+public final class Constants {
+    private Constants(){}
+    public static final String email = "email";
+    public static final String password = "password";
+    public static final String automationTestSiteBaseURL = "https://www.automationtesting.co.uk/";
+}
